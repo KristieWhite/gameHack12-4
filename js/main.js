2b469851b7ce2528cba2f413d8ba2e527074a4c1
Eijah Duane Prince <elijahprince73@gmail.com>
2015-12-04 23:52:45 -0600

    not done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 	console.log("ready!");
 
-/////////////////////Challenges//////////////////////////////////////////////
+	/////////////////////Challenges//////////////////////////////////////////////
 	// $.ajax({
 	// 	url: 'https://cryptic-river-3268.herokuapp.com/api/challengev/'
 	// }).then(function (resp) {
@@ -19,13 +19,20 @@ $(document).ready(function () {
 			"title": "title",
 			"description": "description",
 			"video": "video"
-		}
+		},
+		defaults: {
+			title: null,
+			description: null,
+			video: null
+		},
 	});
 
 	var router = new Router();
+	router.on('route')
 
 	var challengeModel = Backbone.Model.extend({
 		url: 'https://cryptic-river-3268.herokuapp.com/api/challengev/'
+
 	});
 
 	var challengeCollection = Backbone.Collection.extend({
@@ -33,115 +40,110 @@ $(document).ready(function () {
 		url: 'https://cryptic-river-3268.herokuapp.com/api/challengev/'
 	});
 
-	
-						/* Titles */
+
+	/* Titles */
 
 	var challenges = new challengeCollection();
 	challenges.fetch({
 		success: function (resp) {
-			var challengeInfo = {'challenges': resp.toJSON()};
+			var challengeInfo = {
+				'challenges': resp.toJSON()
+			};
 			var challengeTemplate = $("#challengesTemplate").text();
-			var challengesHTML = Mustache.render(challengeTemplate , challengeInfo);
-      console.log('challengesHTML:', challengesHTML);
+			var challengesHTML = Mustache.render(challengeTemplate, challengeInfo);
+
 			$("#challenge").html(challengesHTML);
-			console.log(challengeInfo);
+
+		},
+		error: function (err) {
+			console.log("error", err);
+		}
+
+	});
+
+
+
+	/////////////////////////////Top Rated///////////////////////////////////////
+	// $.ajax({
+	//   url: 'https://cryptic-river-3268.herokuapp.com/api/topchallengev/'
+	// }).then(function (resp) {
+	//   console.log(resp);
+	// });
+
+	var topRateModel = Backbone.Model.extend({
+		url: 'https://cryptic-river-3268.herokuapp.com/api/topchallengev/'
+	});
+
+	var topRateCollection = Backbone.Collection.extend({
+		model: topRateModel,
+		url: 'https://cryptic-river-3268.herokuapp.com/api/topchallengev/'
+	});
+
+	var topRates = new topRateCollection();
+
+	topRates.fetch({
+		success: function (resp) {
+			var topRateInfo = {
+				'topRates': resp.toJSON()
+			};
+			var topRateTemplate = $("#topRatesTemplate").text();
+			var topRatesHTML = Mustache.render(topRateTemplate, topRateInfo);
+
+			$("#top").html(topRatesHTML);
+
 		},
 		error: function (err) {
 			console.log("error", err);
 		}
 	});
-	
-	
 
-/////////////////////////////Top Rated///////////////////////////////////////
-// $.ajax({
-//   url: 'https://cryptic-river-3268.herokuapp.com/api/topchallengev/'
-// }).then(function (resp) {
-//   console.log(resp);
-// });
+	//////////////////////////////Submissions/////////////////////////////////////
+	// $.ajax({
+	//   url: 'https://cryptic-river-3268.herokuapp.com/api/topsubmissionv/'
+	// }).then(function (resp) {
+	//   console.log(resp);
+	// });
 
-var topRateModel = Backbone.Model.extend({
-  url: 'https://cryptic-river-3268.herokuapp.com/api/topchallengev/'
-});
-
-var topRateCollection = Backbone.Collection.extend({
-  model:topRateModel,
-  url: 'https://cryptic-river-3268.herokuapp.com/api/topchallengev/'
-});
-
-var topRates = new topRateCollection();
-
-topRates.fetch({
-  success: function (resp) {
-    var topRateInfo = {'topRates': resp.toJSON()};
-    var topRateTemplate = $("#topRatesTemplate").text();
-    var topRatesHTML = Mustache.render(topRateTemplate , topRateInfo);
-    console.log('topRatesHTML:', topRatesHTML);
-    $("#top").html(topRatesHTML);
-    console.log(topRateInfo);
-  },
-  error: function (err) {
-    console.log("error", err);
-  }
-});
-
-//////////////////////////////Submissions/////////////////////////////////////
-// $.ajax({
-//   url: 'https://cryptic-river-3268.herokuapp.com/api/topsubmissionv/'
-// }).then(function (resp) {
-//   console.log(resp);
-// });
-
-var submissionModel = Backbone.Model.extend({
-  url: 'https://cryptic-river-3268.herokuapp.com/api/topsubmissionv/'
-});
-
-var submissionCollection = Backbone.Collection.extend({
-  model: submissionModel,
-  url: 'https://cryptic-river-3268.herokuapp.com/api/topsubmissionv/'
-});
-
-var submissions = new submissionCollection();
-
-submissions.fetch({
-  success: function (resp) {
-    var submissionInfo = {'submissions': resp.toJSON()};
-    var submissionTemplate = $("#submissionsTemplate").text();
-    var submissionsHTML = Mustache.render(submissionTemplate , submissionInfo);
-    console.log('submissionsHTML:', submissionsHTML);
-    $("#sub").html(submissionsHTML);
-    console.log(submissionInfo);
-  },
-  error: function (err) {
-    console.log("error", err);
-  }
-});
-
-	//////////////////////Log In////////////////////////////////////////////////
-	$(function () {
-		$('#login-form-link').click(function (e) {
-			$("#login-form").delay(100).fadeIn(100);
-			$("#register-form").fadeOut(100);
-			$('#register-form-link').removeClass('active');
-			$(this).addClass('active');
-			e.preventDefault();
-		});
-		$('#register-form-link').click(function (e) {
-			$("#register-form").delay(100).fadeIn(100);
-			$("#login-form").fadeOut(100);
-			$('#login-form-link').removeClass('active');
-			$(this).addClass('active');
-			e.preventDefault();
-		});
+	var submissionModel = Backbone.Model.extend({
+		url: 'https://cryptic-river-3268.herokuapp.com/api/topsubmissionv/'
 	});
 
+	var submissionCollection = Backbone.Collection.extend({
+		model: submissionModel,
+		url: 'https://cryptic-river-3268.herokuapp.com/api/topsubmissionv/'
+	});
+
+	var submissions = new submissionCollection();
+
+	submissions.fetch({
+		success: function (resp) {
+			var submissionInfo = {
+				'submissions': resp.toJSON()
+			};
+			var submissionTemplate = $("#submissionsTemplate").text();
+			var submissionsHTML = Mustache.render(submissionTemplate, submissionInfo);
+
+			$("#sub").html(submissionsHTML);
+
+		},
+		error: function (err) {
+			console.log("error", err);
+		}
+	});
+
+
+
+
 	////////////////////////////////////////////////////////////
-$(".container").hide();
+	$("body").on('click', 'a', function (e) {
+		e.preventDefault();
+		var href = $(this).attr('href');
+		href = href.substr(1);
+		router.navigate(href, {
+			trigger: true
+		});
 
 
-
-	////////////////////////////////////////////////////////////////////////////
-$("form").hide();
-	
+	});
+	$("#formContainer").hide();
 });
-

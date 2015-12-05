b4f0a8ebcdf3f66a4b168f4b87231c45e71fc4f5
Eijah Duane Prince <elijahprince73@gmail.com>
2015-12-04 18:32:43 -0600

    done

--- js/main.js
+++ js/main.js
@@ -1,73 +1,76 @@
 $(document).ready(function () {
 
-    console.log("ready!");
+	console.log("ready!");
 
-$.ajax({
-  url: 'https://cryptic-river-3268.herokuapp.com/api/challengev/'
-}).then(function(resp){
-  console.log(resp);
+	$.ajax({
+		url: 'https://cryptic-river-3268.herokuapp.com/api/challengev/'
+	}).then(function (resp) {
+		console.log(resp);
+	});
+
+	var Router = Backbone.Router.extend({
+		initialize: function () {
+			Backbone.history.start({
+				pushState: true
+			});
+		},
+		routes: {
+			"title": "title",
+			"description": "description",
+			"video": "video"
+		}
+	});
+
+	var router = new Router();
+
+	var challengeModel = Backbone.Model.extend({
+		url: 'https://cryptic-river-3268.herokuapp.com/api/challengev/'
+	});
+	
+	var challengeCollection = Backbone.Collection.extend({
+		model: challengeModel,
+		url: 'https://cryptic-river-3268.herokuapp.com/api/challengev/'
+	});
+	
+	var challenges = new challengeCollection();
+
+	challenges.fetch({
+		success: function (resp) {
+			var challengeInfo = {'challenges': resp.toJSON()};
+			var challengeTemplate = $("#challengesTemplate").text();
+			var challengesHTML = Mustache.render(challengeTemplate , challengeInfo);
+			$("#video").html(challengesHTML); 
+			console.log(challengeInfo);
+		},
+		error: function (err) {
+			console.log("error", err);
+		}
+	});
+
+
+
+	//////////////////////Log In//////////////////////
+	$(function () {
+		$('#login-form-link').click(function (e) {
+			$("#login-form").delay(100).fadeIn(100);
+			$("#register-form").fadeOut(100);
+			$('#register-form-link').removeClass('active');
+			$(this).addClass('active');
+			e.preventDefault();
+		});
+		$('#register-form-link').click(function (e) {
+			$("#register-form").delay(100).fadeIn(100);
+			$("#login-form").fadeOut(100);
+			$('#login-form-link').removeClass('active');
+			$(this).addClass('active');
+			e.preventDefault();
+		});
+	});
+	////////////////////////////////////////////////////////////
+$(".container").hide();
 });
 
-var Router = Backbone.Router.extend({
-  initialize: function() {
-    Backbone.history.start({pushState: true});
-  },
-  routes: {
-    "title": "title",
-    "description": "description",
-    "video": "video"
-  }
-});
 
-var router = new Router();
-
-var challengeContainer = Backbone.Model.extend({
-  initialize: function() {
-  },
-  defaults: {
-    title: null,
-    description: null,
-    video: null
-  },
-  Model: challengeContainer,
-  url: 'https://cryptic-river-3268.herokuapp.com/api/challengev/'
-});
-
-var challengeContainers = Backbone.Collection.extend({
-  Model: challengeContainer
-});
-
-var challengeCollection = new challengeContainer();
-
-challengeCollection.fetch ({
-  success: function(resp) {
-    console.log("success", resp);
-  },
-  error: function(err) {
-    console.log("error", err);
-  }
-});
-
-
-
-//////////////////////Log In//////////////////////
-    $(function () {
-        $('#login-form-link').click(function (e) {
-            $("#login-form").delay(100).fadeIn(100);
-            $("#register-form").fadeOut(100);
-            $('#register-form-link').removeClass('active');
-            $(this).addClass('active');
-            e.preventDefault();
-        });
-        $('#register-form-link').click(function (e) {
-            $("#register-form").delay(100).fadeIn(100);
-            $("#login-form").fadeOut(100);
-            $('#login-form-link').removeClass('active');
-            $(this).addClass('active');
-            e.preventDefault();
-        });
-    });
-////////////////////////////////////////////////////////////
-
-});
-
+var challengeModel = Backbone.Model.extend({
+	
+})
